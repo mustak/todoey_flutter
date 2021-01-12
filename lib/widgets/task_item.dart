153ed1890a4380171afe25b73103a1d09db6840d
mustak <mhyaqub@yahoo.com>
2021-01-12 17:56:13 +0000

    [202] Add ListView Builder

--- lib/widgets/task_item.dart
+++ lib/widgets/task_item.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 
 class TaskItem extends StatelessWidget {
   final String text;
-  final bool value;
+  final bool isChecked;
+  final Function changeState;
 
-  const TaskItem({@required this.text, this.value});
+  const TaskItem({@required this.text, this.isChecked, this.changeState});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(text),
+      title: Text(
+        text,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null,
+            decorationColor: Colors.red.withAlpha(120),
+            decorationThickness: 4.0),
+      ),
       leading: Checkbox(
-        value: value,
-        onChanged: (value) {},
+        activeColor: Colors.lightBlue,
+        value: isChecked,
+        onChanged: (value) => changeState(),
       ),
     );
   }
--- lib/widgets/task_items.dart
+++ lib/widgets/task_items.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
 
 import 'package:todoey_flutter/widgets/task_item.dart';
+import 'package:todoey_flutter/models/task.dart';
 
-class TaskItems extends StatelessWidget {
-  const TaskItems();
+class TaskItems extends StatefulWidget {
+  @override
+  _TaskItemsState createState() => _TaskItemsState();
+}
+
+class _TaskItemsState extends State<TaskItems> {
+  List<Task> tasks = [
+    Task(title: 'List Item 01', isDone: false),
+    Task(title: 'List Item 02', isDone: true),
+    Task(title: 'List Item 03', isDone: false),
+  ];
+
+  void toggleCheckbox(int i) {
+    setState(() {
+      tasks[i].toggle();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskItem(text: 'List Item 01', value: false),
-        TaskItem(text: 'List Item 02', value: false),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, i) {
+        return TaskItem(
+          text: tasks[i].title,
+          isChecked: tasks[i].isDone,
+          changeState: () => setState(() {
+            tasks[i].toggle();
+          }),
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
