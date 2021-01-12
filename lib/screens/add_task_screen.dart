e73083e8d8b41a8c6c81a6d9a51b9b0eed2bf947
mustak <mhyaqub@yahoo.com>
2021-01-12 18:29:31 +0000

    Re-design add screen UI

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -9,11 +9,9 @@ class AddTaskScreen extends StatelessWidget {
         left: 20,
         right: 20,
       ),
-      padding: EdgeInsets.only(
-        left: 30,
-        right: 30,
-        top: 30,
-        bottom: 10,
+      padding: EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 20,
       ),
       decoration: BoxDecoration(
         color: Colors.white,
@@ -23,29 +21,36 @@ class AddTaskScreen extends StatelessWidget {
         ),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(
-            'Add New Task',
-            style: TextStyle(
-              fontSize: 28,
-              color: Colors.blue,
+          // Text(
+          //   'Add New Task',
+          //   textAlign: TextAlign.center,
+          //   style: TextStyle(
+          //     fontSize: 20,
+          //     color: Colors.blue,
+          //   ),
+          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
             ),
-          ),
-          TextField(
-            style: TextStyle(fontSize: 20),
-            autofocus: true,
-            textAlign: TextAlign.center,
-            decoration: InputDecoration(
-                hintText: 'Enter a task name',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 0),
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                  gapPadding: 10,
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 0,
-                  vertical: 0,
-                )),
+            child: TextField(
+              style: TextStyle(fontSize: 20),
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  hintText: 'Enter new task',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0),
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    gapPadding: 10,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 0,
+                    vertical: 0,
+                  )),
+            ),
           ),
           FlatButton(
             onPressed: () {},
